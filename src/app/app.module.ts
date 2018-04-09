5aa4e4e088eb44aa07abf1c56a7489ff8cc26f08
Çelik <umit.celik@capgemini.com>
2018-04-09 13:20:24 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListComponent } from './list/list.component';
+import { ListCardComponent } from './list-card/list-card.component';
+import { SearchFilterComponent } from './search-filter/search-filter.component';
+import {FormsModule} from '@angular/forms';
+import {MultiselectDropdownModule} from 'angular-2-dropdown-multiselect';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ListComponent,
+    ListCardComponent,
+    SearchFilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MultiselectDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
